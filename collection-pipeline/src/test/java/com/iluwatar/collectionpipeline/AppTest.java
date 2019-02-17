5b16c5e4ed92a3c2c5b1c1129ec1aec33354d353
Marcus Granström <marcus.granstroem@gmail.com>
2019-02-17 13:15:39 +0100

    add coverage measuring to test

--- collection-pipeline/src/test/java/com/iluwatar/collectionpipeline/AppTest.java
+++ collection-pipeline/src/test/java/com/iluwatar/collectionpipeline/AppTest.java
@@ -1,17 +1,17 @@
 /**
  * The MIT License
  * Copyright (c) 2014 Ilkka Seppälä
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -39,7 +39,6 @@ public class AppTest {
 
   private List<Car> cars = CarFactory.createCars();
 
-
   @BeforeAll
   public static void beforeAll() {
     int branches = 12;
@@ -52,6 +51,9 @@ public class AppTest {
     Arrays.fill(Car.visitedBranchLocal, false);
   }
 
+  /**
+   * Runs after each test. Adds newly covered branches to global coverage.
+   */
   @AfterEach
   public void afterEach() {
     boolean verbose = false;
@@ -68,17 +70,20 @@ public class AppTest {
     }
   }
 
+  /**
+   * Prints global test coverage for whole test suite.
+   */
   @AfterAll
   public static void afterAll() {
     int cover = 0;
     for (int i = 0; i < Car.visitedBranchGlobal.length; i++) {
-      System.out.format("Branch %d covered: %b.\n", i, Car.visitedBranchGlobal[i]);
+      System.out.format("Branch %d covered: %b.\n", i + 1, Car.visitedBranchGlobal[i]);
       if (Car.visitedBranchGlobal[i]) {
         cover++;
       }
     }
     System.out.println("Test done, results:");
-    System.out.format("Branch cover is: %f.\n", (double) cover/Car.visitedBranchGlobal.length);
+    System.out.format("Branch cover is: %f.\n", (double) cover / Car.visitedBranchGlobal.length);
   }
 
   @Test
@@ -86,13 +91,13 @@ public class AppTest {
     List<String> models = ImperativeProgramming.getModelsAfter2000(cars);
     assertEquals(Arrays.asList("Avenger", "Wrangler", "Focus", "Cascada"), models);
   }
-  
+
   @Test
   public void testGetModelsAfter2000UsingPipeline() {
     List<String> models = FunctionalProgramming.getModelsAfter2000(cars);
     assertEquals(Arrays.asList("Avenger", "Wrangler", "Focus", "Cascada"), models);
   }
-  
+
   @Test
   public void testGetGroupingOfCarsByCategory() {
     Map<Category, List<Car>> modelsExpected = new HashMap<>();
@@ -108,28 +113,15 @@ public class AppTest {
     assertEquals(modelsExpected, modelsFunctional);
     assertEquals(modelsExpected, modelsImperative);
   }
-  
+
   @Test
   public void testGetSedanCarsOwnedSortedByDate() {
     Person john = new Person(cars);
-    List<Car> modelsExpected = Arrays.asList(new Car("Dodge", "Avenger", 2010, Category.SEDAN), 
+    List<Car> modelsExpected = Arrays.asList(new Car("Dodge", "Avenger", 2010, Category.SEDAN),
         new Car("Ford", "Focus", 2012, Category.SEDAN));
     List<Car> modelsFunctional = FunctionalProgramming.getSedanCarsOwnedSortedByDate(Arrays.asList(john));
     List<Car> modelsImperative = ImperativeProgramming.getSedanCarsOwnedSortedByDate(Arrays.asList(john));
     assertEquals(modelsExpected, modelsFunctional);
     assertEquals(modelsExpected, modelsImperative);
-  }
-
-  @Test
-  public void testEqualsSelf() {
-    Car car = new Car("Toyota", "Prius", 2011, Category.SEDAN);
-    Assert.assertTrue(car.equals(car));
-  }
-
-  @Test
-  public void testOneCarNullMaker() {
-    Car car1 = new Car(null,"Matrix", 2001,Category.JEEP);
-    Car car2 = new Car("Mazzerutzi","Matrix", 2001,Category.JEEP);
-    Assert.assertFalse(car1.equals(car2));
   }
 }
