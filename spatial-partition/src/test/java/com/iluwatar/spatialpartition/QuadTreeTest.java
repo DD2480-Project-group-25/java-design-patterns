e282aa785b1c932ce811b515b3aa9384ffcfba7f
vronc <34446119+vronc@users.noreply.github.com>
2019-02-18 10:56:32 +0100

    Coverage for QuadTree.insert() - Fix #11 (#34)
    * Add tracking of branch coverage and print out result when tests are run.
    
    * Correction of branch counting in QuadTree.insert()
    
    * Refactoring tracking of covered branches in QuadTree.insert()
    
    * Refactoring tracking of covered branches again.

--- spatial-partition/src/test/java/com/iluwatar/spatialpartition/QuadTreeTest.java
+++ spatial-partition/src/test/java/com/iluwatar/spatialpartition/QuadTreeTest.java
@@ -25,15 +25,50 @@ package com.iluwatar.spatialpartition;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Random;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 /**
  * Testing QuadTree class.
  */
 
 class QuadTreeTest {
+
+  @BeforeAll
+  public static void setCoveredBranches() {
+    QuadTree.coveredBranches = new boolean[8];
+    QuadTree.localCoveredBranches = new boolean[8];
+  }
+
+  @AfterAll
+    public static void printCoveredBranches() {
+    System.out.println(Arrays.toString(QuadTree.coveredBranches));
+    int count = 0;
+    for (int i = 0 ; i < QuadTree.coveredBranches.length; i++) {
+      if (QuadTree.coveredBranches[i]) {
+        count++;
+      }
+    }
+    float fraction = (float) count / QuadTree.coveredBranches.length;
+    System.out.println("\n" + fraction * 100 + "% branch coverage\n");
+  }
+
+  @BeforeEach
+  public void initLocalCoveredBranches() {
+    Arrays.fill(QuadTree.localCoveredBranches, false);
+  }
+
+  @AfterEach
+  public void addCoveredBranches() {
+    for (int i = 0 ; i < QuadTree.coveredBranches.length ; i++) {
+      if (QuadTree.localCoveredBranches[i]) {
+        QuadTree.coveredBranches[i] = true;
+      }
+    }
+  }
 
   @Test
   void queryTest() {
